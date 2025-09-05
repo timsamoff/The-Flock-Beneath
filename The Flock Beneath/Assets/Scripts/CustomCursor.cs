4396264cfd0645f0e45e0aab45d4bcd7c601236c
Tim Samoff <samoff@gmail.com>
2025-09-04 19:33:39 -0700

    Fixed a cursor issue / new WebGL Build

--- The Flock Beneath/Assets/Scripts/CustomCursor.cs
+++ The Flock Beneath/Assets/Scripts/CustomCursor.cs
@@ -4,9 +4,33 @@ using UnityEngine.SceneManagement;
 public class CustomCursor : MonoBehaviour
 {
     [SerializeField] private Texture2D cursorTexture;
-
+    
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SetCustomCursor();
+    }
+    
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+        SetCustomCursor();
+    }
+    
+    private void SetCustomCursor()
+    {
+        if (cursorTexture != null)
+        {
+            Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+        }
+    }
+    
+    private void Start()
+    {
+        SetCustomCursor();
     }
 }
